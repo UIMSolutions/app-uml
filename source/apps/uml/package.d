58c226bfc4dde5785895f018e5ab01fba7f57a28
Sicherheitsschmiede <ui.manufaktur@gmail.com>
2023-05-15 03:43:08 +0200

    Updates

--- source/apps/uml/package.d
+++ source/apps/uml/package.d
@@ -1,5 +1,33 @@
 module apps.uml;
 
-public import apps.uml.controllers;
-public import apps.uml.routers;
-public import apps.uml.views;
+mixin(ImportPhobos!());
+
+// Dub
+public import vibe.d;
+
+// UIM
+public import uim.core;
+public import uim.bootstrap;
+public import uim.html;
+public import uim.oop;
+public import uim.models;
+public import uim.apps;
+public import uim.controls;
+public import uim.servers;
+
+public import langs.javascript;
+
+public {
+  import apps.uml.controllers;
+  import apps.uml.helpers;
+  import apps.uml.tests;
+  import apps.uml.views;
+}
+
+DApplication umlApp;
+static this() {
+  portalsApp = Application
+    .name("umlApp")
+    .rootPath("/apps/uml")
+    .addRoute(Route("/", HTTPMethod.GET, umlIndexPageController));
+}
